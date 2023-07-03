e466effb590be547e26ad3b915ca4f285b6d2dcd
Ben Epstein <ben.epstein97@gmail.com>
2023-07-03 15:02:11 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,20 @@
 from datetime import datetime
 import streamlit as st
 
+# Day we started
+start = datetime(year=2023, month=6, day=29)
+
 # Get today's date
 today = datetime.today().date()
 
 # Specify the future date
-future_date = datetime(2024, 3, 1).date()  # Replace with your desired future date
+future_date = datetime(2024, 3, 1).date() 
 
 # Calculate the number of days
 num_days = (future_date - today).days
+days_since_start = future_date - start).days
 
 
 st.title("FranzCountdown")
-st.metric(label="Remaining", value=f"{num_days} days", delta="-1 day")
+st.metric(label="Remaining", value=f"{num_days} days", delta=f"-{days_since_start} days")
 
